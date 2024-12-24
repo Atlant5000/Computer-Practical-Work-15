b2d1286f31f04897a7da6a20a7ddef5601cecf37
Maksim Mihailovskii <maximmmd13@gmail.com>
2024-12-24 09:44:26 +0200

    Add files via upload

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    /// <summary>
+    /// Генерируются 15 случайных целых чисел в интервале (-20, 30). Вывести эти числа и подсчитать количество положительных чисел.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        public static void Func()
+        {
+            int b = 0;
+            Random number= new Random();
+            for (int x = 0; x < 15; x++)
+            {
+                int a = number.Next(-20, 30);
+                Console.WriteLine(a);
+                if (a > 0) 
+                {
+                   b++;
+                }
+            }
+            Console.WriteLine($"Количество положительных чисел={b}");
+        }
+    }
+}
