032da527c7535543644b29b966ad6873b567466d
Maksim Mihailovskii <maximmmd13@gmail.com>
2024-12-24 08:59:10 +0200

    Add files via upload

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    /// <summary>
+    /// Напечатать ряд чисел 20 в виде: 20 20 20 20 20 20 20 20 20 20.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        public static void Func()
+        {
+            int x = 0;
+            while (x < 11) 
+            {
+                Console.Write(" 20");
+                x++;
+            }
+        }
+    }
+}
