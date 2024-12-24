f9426a14962e49d942abc2cf5cd178044f7635a6
Maksim Mihailovskii <maximmmd13@gmail.com>
2024-12-24 09:43:42 +0200

    Add files via upload

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    /// <summary>
+    /// Генерируются 10 случайных целых чисел в интервале (-40, 40). Вывести эти числа.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        public static void Func()
+        {
+            Random a = new Random();
+            for (int i = 0;i<10;i++)
+            {
+                int number = a.Next(-40, 40);
+                Console.WriteLine(number);  
+            }
+        }
+    }
+}
