67e7f029a313cec722487743ee76205ebacd1c6c
Maksim Mihailovskii <maximmmd13@gmail.com>
2024-12-24 09:17:29 +0200

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    /// <summary>
+    /// Вывести четные двузначные числа и подсчитать их количество.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        public static void Func()
+        {
+            int a = 0;
+            for(int i=10;i<=99;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);a++;
+                }
+            }
+            Console.WriteLine($"a={a}");
+        }
+    }
+}
